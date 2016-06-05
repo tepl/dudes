cd810589ecb0094e79aa96ced8968329ad67d5d6
tepl <tepl@users.noreply.github.com>
2016-06-05 00:33:00 -0500

    Initial commit

---
+++ src/com/tearulez/dudes/Character.java
@@ -0,0 +1,10 @@
+package com.tearulez.dudes;
+
+/**
+ * Created by Teplovoz on 6/4/2016.
+ */
+public class Character {
+    public String name;
+    public String otherStuff;
+    public int id, x, y;
+}
