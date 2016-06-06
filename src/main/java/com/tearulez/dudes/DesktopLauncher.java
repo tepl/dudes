87b90860db3c67f70491d23afce7619ce1683e1a
tepl <tepl@users.noreply.github.com>
2016-06-06 14:22:30 -0500

    Add libGDX empty app

---
+++ src/main/java/com/tearulez/dudes/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.tearulez.dudes;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Drop";
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new ApplicationAdapter() {
+        }, config);
+    }
+}
