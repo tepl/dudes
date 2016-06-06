eaed65ff6435af17930ea7aea28fbbe112f202b9
tepl <tepl@users.noreply.github.com>
2016-06-06 14:48:31 -0500

    Add moving circle with arrow keys

--- src/main/java/com/tearulez/dudes/GameAdapter.java
+++ src/main/java/com/tearulez/dudes/GameAdapter.java
@@ -2,12 +2,14 @@ package com.tearulez.dudes;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class GameAdapter extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer;
+    private int x = 0, y = 0;
 
     @Override
     public void create() {
@@ -19,9 +21,14 @@ public class GameAdapter extends ApplicationAdapter {
         Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) x -= 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) x += 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) y += 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) y -= 1;
+
         shapeRenderer.setColor(Color.BLACK);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.circle(50, 50, 32);
+        shapeRenderer.circle(400 + x, 240 + y, 32);
         shapeRenderer.end();
 
     }
